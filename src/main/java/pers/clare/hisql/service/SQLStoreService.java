22a9fb971bf5a668ee647cca932479321d5a8989
clare <babyblue94520@gamil.com>
2023-08-21 10:44:13 +0800

    optimize(SQLStoreService): merge array and collection method.

--- src/main/java/pers/clare/hisql/service/SQLStoreService.java
+++ src/main/java/pers/clare/hisql/service/SQLStoreService.java
@@ -11,6 +11,7 @@ import java.lang.reflect.Field;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Arrays;
 import java.util.Collection;
 
 
@@ -34,51 +35,33 @@ public class SQLStoreService extends SQLStorePageService {
         }
     }
 
-    public <T> Collection<T> insertAll(
-            SQLCrudStore<T> sqlStore
-            , Collection<T> entities
-    ) {
-        return this.connection((connection) -> {
-            Field autoKey = sqlStore.getAutoKey();
-            if (autoKey == null) {
-                for (T entity : entities) {
-                    SQLData sqlData = SQLStoreUtil.toInsertSQLData(sqlStore, entity);
-                    PreparedStatement statement = connection.prepareStatement(sqlData.getSql());
-                    ConnectionUtil.insert(statement, sqlData.getParameters());
-                }
-            } else {
-                ResultSet rs;
-                for (T entity : entities) {
-                    SQLData sqlData = SQLStoreUtil.toInsertSQLData(sqlStore, entity);
-                    PreparedStatement statement = connection.prepareStatement(sqlData.getSql(), Statement.RETURN_GENERATED_KEYS);
-                    ConnectionUtil.insert(statement, sqlData.getParameters());
-                    rs = statement.getGeneratedKeys();
-                    autoKey.set(entity, rs.next() ? rs.getObject(1, autoKey.getType()) : null);
-                }
-            }
-            return entities;
-        });
-    }
-
     public <T> T[] insertAll(
             SQLCrudStore<T> sqlStore
             , T[] entities
     ) {
+        if (entities != null && entities.length > 0) {
+            insertAll(sqlStore, Arrays.asList(entities));
+        }
+        return entities;
+    }
+
+    public <T> Collection<T> insertAll(
+            SQLCrudStore<T> sqlStore
+            , Collection<T> entities
+    ) {
+        if (entities == null || entities.size() == 0) return entities;
         return this.connection((connection) -> {
             Field autoKey = sqlStore.getAutoKey();
             if (autoKey == null) {
                 for (T entity : entities) {
                     SQLData sqlData = SQLStoreUtil.toInsertSQLData(sqlStore, entity);
-                    PreparedStatement statement = connection.prepareStatement(sqlData.getSql());
-                    ConnectionUtil.insert(statement, sqlData.getParameters());
+                    ConnectionUtil.update(connection, sqlData.getSql(), sqlData.getParameters());
                 }
             } else {
-                ResultSet rs;
                 for (T entity : entities) {
                     SQLData sqlData = SQLStoreUtil.toInsertSQLData(sqlStore, entity);
-                    PreparedStatement statement = connection.prepareStatement(sqlData.getSql(), Statement.RETURN_GENERATED_KEYS);
-                    ConnectionUtil.insert(statement, sqlData.getParameters());
-                    rs = statement.getGeneratedKeys();
+                    Statement statement = ConnectionUtil.insert(connection, sqlData.getSql(), sqlData.getParameters());
+                    ResultSet rs = statement.getGeneratedKeys();
                     autoKey.set(entity, rs.next() ? rs.getObject(1, autoKey.getType()) : null);
                 }
             }
@@ -98,6 +81,14 @@ public class SQLStoreService extends SQLStorePageService {
         }
     }
 
+    public final <T> int[] updateAll(
+            SQLCrudStore<T> sqlStore
+            , T[] entities
+    ) {
+        if (entities == null || entities.length == 0) return new int[0];
+        return updateAll(sqlStore, Arrays.asList(entities));
+    }
+
     public <T> int[] updateAll(
             SQLCrudStore<T> sqlStore
             , Collection<T> entities
@@ -108,30 +99,21 @@ public class SQLStoreService extends SQLStorePageService {
             int i = 0;
             for (T entity : entities) {
                 SQLData sqlData = SQLStoreUtil.toUpdateSQLData(sqlStore, entity);
-                PreparedStatement statement = connection.prepareStatement(sqlData.getSql());
-                counts[i++] = ConnectionUtil.update(statement, sqlData.getParameters());
+                PreparedStatement ps = connection.prepareStatement(sqlData.getSql());
+                ConnectionUtil.setUpdateValue(ps, sqlData.getParameters());
+                counts[i++] = ps.executeUpdate();
             }
             return counts;
         });
     }
 
-    public final <T> int[] updateAll(
+    public <T> int[] deleteAll(
             SQLCrudStore<T> sqlStore
             , T[] entities
     ) {
         if (entities == null || entities.length == 0) return new int[0];
-        return this.connection((connection) -> {
-            int[] counts = new int[entities.length];
-            int i = 0;
-            for (T entity : entities) {
-                SQLData sqlData = SQLStoreUtil.toUpdateSQLData(sqlStore, entity);
-                PreparedStatement statement = connection.prepareStatement(sqlData.getSql());
-                counts[i++] = ConnectionUtil.update(statement, sqlData.getParameters());
-            }
-            return counts;
-        });
+        return deleteAll(sqlStore, Arrays.asList(entities));
     }
-
 
     public <T> int[] deleteAll(
             SQLCrudStore<T> sqlStore
@@ -140,21 +122,6 @@ public class SQLStoreService extends SQLStorePageService {
         if (entities == null || entities.size() == 0) return new int[0];
         return this.connection((connection) -> {
             int[] counts = new int[entities.size()];
-            int i = 0;
-            for (T entity : entities) {
-                counts[i++] = update(SQLQueryUtil.setValue(sqlStore.getDeleteById(), sqlStore.getKeyFields(), entity));
-            }
-            return counts;
-        });
-    }
-
-    public <T> int[] deleteAll(
-            SQLCrudStore<T> sqlStore
-            , T[] entities
-    ) {
-        if (entities == null || entities.length == 0) return new int[0];
-        return this.connection((connection) -> {
-            int[] counts = new int[entities.length];
             int i = 0;
             for (T entity : entities) {
                 counts[i++] = update(SQLQueryUtil.setValue(sqlStore.getDeleteById(), sqlStore.getKeyFields(), entity));
