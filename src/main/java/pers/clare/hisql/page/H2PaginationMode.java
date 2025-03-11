3a75822f867bb720a151f49a178a325d7ad96581
clare <babyblue94520@yahoo.com.tw>
2025-03-11 16:51:14 +0800

    feat: add H2PaginationMode.

---
+++ src/main/java/pers/clare/hisql/page/H2PaginationMode.java
@@ -0,0 +1,45 @@
+package pers.clare.hisql.page;
+
+import pers.clare.hisql.exception.HiSqlException;
+import pers.clare.hisql.util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@SuppressWarnings("unused")
+public class H2PaginationMode implements PaginationMode {
+    private final Pattern scanCountPattern = Pattern.compile("scanCount: (\\d+)");
+
+    public void appendPaginationSQL(
+            StringBuilder sql
+            , Pagination pagination
+    ) {
+        appendSortSQL(sql, pagination.getSorts());
+        sql.append(" limit ")
+                .append(pagination.getSize() * pagination.getPage())
+                .append(',')
+                .append(pagination.getSize());
+    }
+
+    @Override
+    public long getVirtualTotal(
+            Pagination pagination
+            , Connection connection
+            , String sql
+            , Object[] parameters
+    ) throws SQLException {
+        ResultSet rs = ConnectionUtil.query(connection, "explain analyze " + sql, parameters);
+        if (rs.next()) {
+            String plan = rs.getString(1);
+            Matcher matcher = scanCountPattern.matcher(plan);
+            if (matcher.find()) {
+                String countString = matcher.group(1);
+                return Long.parseLong(countString);
+            }
+        }
+        throw new HiSqlException(String.format("query total error.(%s)", sql));
+    }
+}
--- src/test/java/pers/clare/hisql/data/HiSqlConfig.java
+++ src/test/java/pers/clare/hisql/data/HiSqlConfig.java
@@ -1,10 +1,12 @@
 package pers.clare.hisql.data;
 
 import pers.clare.hisql.annotation.EnableHiSql;
+import pers.clare.hisql.page.H2PaginationMode;
 
 @EnableHiSql(
         resultSetConverter = CustomResultSetConverter.class
         , beanNamePrefix = "test"
+        , paginationMode = H2PaginationMode.class
 )
 public class HiSqlConfig {
 }
